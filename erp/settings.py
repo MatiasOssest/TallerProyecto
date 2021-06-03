02114952d1d7462c2c7911c23aab813af838c2bf
acaviedes15 <alonso.caviedes@ug.uchile.cl>
2021-06-02 21:33:22 -0400

    Update settings.py
    Se añade la app 'inventario' a Instaled Apps

--- erp/settings.py
+++ erp/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    
+    #custom apps
+    'inventario',
+
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
